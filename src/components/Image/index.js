3e82f2d65853c13cd708c232d161226924eb0332
amanjadon54 <amanjadon54@gmail.com>
2021-04-02 00:45:35 +0530

    core: added default missing image

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 
 class Image extends Component {
+  addDefaultSrc(ev) {
+    ev.target.src = process.env.PUBLIC_URL + "/image-not-found.jpeg";
+  }
+
   render() {
     return (
       <img
         class={this.props.imageClassName}
         src={this.props.imageSrc}
         alt={this.props.imageAlt}
+        onError={this.addDefaultSrc}
       />
     );
   }
