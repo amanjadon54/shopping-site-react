f9ac03cd3220a8526fe702d684f84a30e4e20acf
amanjadon54 <amanjadon54@gmail.com>
2021-04-01 17:15:48 +0530

    Core: added input field for search bar

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class SearchBar extends React.Component {
+  handleEnter = (event) => {
+    if (event.keyCode === 13) {
+      const { handleSearchBarEnter } = this.props;
+      handleSearchBarEnter(event);
+    }
+  };
+
+  render() {
+    return (
+      <input
+        className={this.props.searchBarClassName}
+        value={this.props.searchBarValue}
+        onKeyDown={this.handleEnter}
+      />
+    );
+  }
+}
+
+export default SearchBar;
